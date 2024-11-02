a5c5e56a80c4e28a4bdde31bd7cf53fc9b409393
Taraneh Nesari <taraneh.nesari@gmail.com>
2024-11-02 03:47:01 +0330

    Add project files.

---
+++ PubSubSample/NotificationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubSubSample
+{
+    public class NotificationEvent
+    {
+
+        public string NotificationMessage { get; private set; }
+
+        public DateTime NotificationDate { get; private set; }
+
+        public NotificationEvent(DateTime _dateTime, string _message)
+        {
+            NotificationDate = _dateTime;
+            NotificationMessage = _message;
+        }
+
+    }
+
+}
